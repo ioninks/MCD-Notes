5cc46eb3ad29cfbfb843140ea293ca3b9494c86a
Konstantin Ionin <int4@me.com>
2020-02-26 11:38:02 +0500

    05 core data manager created

---
+++ MCD-Notes/Managers/CoreDataManager.swift
@@ -0,0 +1,85 @@
+//
+//  CoreDataManager.swift
+//  MCD-Notes
+//
+//  Created by Konstantin Ionin on 26.02.2020.
+//  Copyright © 2020 Konstantin Ionin. All rights reserved.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    
+    // MARK: - Properties
+    
+    private let modelName: String
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        // Initialize Managed Object Context
+        let managedObjectContext = NSManagedObjectContext(
+            concurrencyType: .mainQueueConcurrencyType
+        )
+        // Configure Managed Object Context
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        // Fetch Model URL
+        guard let modelURL = Bundle.main.url(
+            forResource: self.modelName,
+            withExtension: "momd"
+        ) else {
+            fatalError("Unable to Find Data Model")
+        }
+        
+        // Load Data Model
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // Initialize Persistend Store Coordinator
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(
+            managedObjectModel: self.managedObjectModel
+        )
+        // Helpers
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        // URL Documents Directory
+        let documentsURL = fileManager.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        )[0]
+        
+        // URL Persistent Store
+        let persistentStoreURL = documentsURL.appendingPathComponent(storeName)
+        
+        do {
+            let options = [
+                NSMigratePersistentStoresAutomaticallyOption: true,
+                NSInferMappingModelAutomaticallyOption: true
+            ]
+            try persistentStoreCoordinator.addPersistentStore(
+                ofType: NSSQLiteStoreType,
+                configurationName: nil,
+                at: persistentStoreURL,
+                options: options
+            )
+        } catch {
+            fatalError("Unable to Find Persistent Store")
+        }
+        return persistentStoreCoordinator
+    }()
+    
+    // MARK: - Initialization
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+}
+
